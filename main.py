2f562af5f58b471e8cd1abf12c074e42b3a3d4f7
marcotcm <marcotcm20@gmail.com>
2026-07-13 01:33:10 -0400

    render

--- main.py
+++ main.py
@@ -24,5 +24,3 @@ app.include_router(api_router, prefix="/api/v1")
 def health_check():
     return {"status": "online", "message": "API RCM operativa"}
 
-if __name__ == "__main__":
-     uvicorn.run(app, host="0.0.0.0", port=8000)
